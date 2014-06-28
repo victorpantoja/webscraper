7aa501684086558e7a20a8731c6d243248bf9b2a
Victor Pantoja <victor.pantoja@gmail.com>
2014-06-28 16:59:09 -0300

    wrong logging config for production

--- webscraper/settings_prod.py
+++ webscraper/settings_prod.py
@@ -4,16 +4,3 @@ PORT = 9085
 
 SERVER_NAME = 'http://victorpantoja.com'
 
-LOGGING['root']['handlers'] = ['file']
-LOGGING['handlers']['file'] = {
-    'level': 'INFO',
-    'class': 'logging.FileHandler',
-    'formatter': 'detailed',
-    'filename': '/var/log/webscraper/app.log',
-    'encoding': 'utf-8',
-}
-
-MEMCACHE = {
-    'servers': ('wedding_plattform.3dpyc9.cfg.use1.cache.amazonaws.com:11211',),
-    'socket_timeout': 1,
-}
